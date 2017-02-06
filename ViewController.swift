f04938eddd42e26d849d334b0f98559f9aa3f13a
gzt0605 <zhongting.ge@ssfs.org>
2017-02-06 11:51:47 -0500

    comment added

--- ViewController.swift
+++ ViewController.swift
@@ -1,3 +1,10 @@
+// Ryan Ge
+//Feb.6, 2016
+//WordCollage.app
+
+
+
+
 /*
 
 This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike
@@ -18,18 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+//make the background color to black
     @IBAction func changeBackgroundColorToBlack(_ sender: UIButton) {
         view.backgroundColor = UIColor.black
     }
-
+//make the background color to white
     @IBAction func changeBackgroundToWhite(_ sender: UIButton) {
         view.backgroundColor = UIColor.white
     }
-    
+//make the background color to magenta
     @IBAction func changeBackgroundColorToMagenta(_ sender: UIButton) {
         view.backgroundColor = UIColor.magenta
     }
+//make the background color to green
     @IBAction func changeBackgroundColorToRGB(_ sender: UIButton) {
         view.backgroundColor = UIColor(red:0.08, green:0.51, blue:0.41, alpha:1.0)
     }
